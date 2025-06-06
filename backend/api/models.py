7196ffa647e16e1baf4754f169e01984c55faf6a
sougato-ghosh <aunkur00@gmail.com>
2025-06-06 10:16:15 +0600

    minor changes

--- backend/api/models.py
+++ backend/api/models.py
@@ -6,6 +6,7 @@ class Note(models.Model):
     title = models.CharField(max_length=256)
     using_date = models.DateField()
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.title
